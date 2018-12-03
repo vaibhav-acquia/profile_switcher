0815a5eff46d9385022e0174f51011717a803728
brooke_heaton <brooke_heaton@402231.no-reply.drupal.org>
2018-12-03 20:47:11 +0000

    Issue #2996786 by brooke_heaton: Added port of Drush command to Drush 9.

---
+++ src/Commands/ProfileSwitcherCommands.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\profile_switcher\Commands;
+
+use Drush\Commands\DrushCommands;
+
+/**
+ * A Drush commandfile for Profile Switcher module.
+ */
+class ProfileSwitcherCommands extends DrushCommands {
+
+  /**
+   * Switch Drupal profile in a installed site
+   *
+   * @param string $profile_to_install
+   *   The profile to activate.
+   *
+   * @command switch:profile
+   * @aliases sp,switch-profile
+   */
+  public function profile($profile_to_install) {
+    $this->output()->writeln('Current profile: ' . \Drupal::installProfile());
+    $this->output()->writeln('Switching profile to ' . $profile_to_install . '!');
+    \Drupal::configFactory()->getEditable('core.extension')
+      ->set('profile', $profile_to_install)
+      ->save();
+    drupal_flush_all_caches();
+
+    $this->output()->writeln('Profile changed to: ' . \Drupal::installProfile());
+  }
+}
